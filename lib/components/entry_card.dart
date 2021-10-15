9efb0b780e77264ab637924d0182f8dfa2ed5903
Vinicius Quaresma da Luz <viniciusquaresma1904@gmail.com>
2021-10-15 09:23:31 -0300

    Visual modification on the entry card

--- lib/components/entry_card.dart
+++ lib/components/entry_card.dart
@@ -10,29 +10,42 @@ class EntryCard extends StatelessWidget {
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 16),
       padding: const EdgeInsets.symmetric(horizontal: 8),
+      width: MediaQuery.of(context).size.width,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(children: [
-            const Icon(Icons.access_time_filled),
-            const Text('09:41 AM - Hoje'),
-            Expanded(
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: const [
-                  Icon(Icons.star_rounded),
-                  Icon(Icons.star_rounded),
-                  Icon(Icons.star_rounded),
-                  Icon(Icons.star_rounded),
-                  Icon(Icons.star_outline_rounded),
-                ]))
-          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.access_time_filled),
+                  const SizedBox(width: 8),
+                  Text('09:41 AM - Hoje',
+                      style: Theme.of(context).textTheme.bodyText1)
+                ],
+              ),
+              IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.more_horiz_rounded))
+            ],
+          ),
+          Row(
+            children: [
+              const Icon(Icons.sentiment_very_satisfied_rounded),
+              const SizedBox(width: 8),
+              Text(
+                'Bem',
+                style: Theme.of(context).textTheme.headline6,
+              )
+            ],
+          ),
           const SizedBox(height: 16),
           if (_entry.title != null)
-            Text(_entry.title!, style: Theme.of(context).textTheme.headline4),
+            Text(_entry.title!, style: Theme.of(context).textTheme.headline6),
           if (_entry.content != null)
-            Text(_entry.content!, style: Theme.of(context).textTheme.bodyText1),
+            Text(_entry.content!, style: Theme.of(context).textTheme.bodyText2),
           const SizedBox(height: 32),
           Divider(
             endIndent: MediaQuery.of(context).size.width * 0.8,
